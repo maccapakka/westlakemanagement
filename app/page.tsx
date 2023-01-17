7ff7dacbf649a6ca2e053663fb85f07ea0747dc6
mccanns <steviemccann@rentalcars.com>
2023-01-17 02:14:34 +0000

    feat: add ISR support

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+export const revalidate = 60;
 
 const footerContent = [
   {
