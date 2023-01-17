7f578631693270de6c7b53af6f5f11e05cc97a50
mccanns <steviemccann@rentalcars.com>
2023-01-17 21:17:36 +0000

    fix: address file name error

--- app/header.tsx
+++ app/header.tsx
@@ -141,3 +141,5 @@ export const Header = () => {
     </>
   );
 };
+
+export default Header;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { Header } from "./Header";
+import Header from "./Header";
 import { navigationItems as footerNav } from "./navigationItems";
 
 import "./globals.css";
